cf1814247dd39b10a525b1fe1dca05207d27040e
mschirbel <mschirbel.g@gmail.com>
2019-05-05 13:39:28 -0300

    changing teams on sql script

--- httpd+mysql/database/setup.sql
+++ httpd+mysql/database/setup.sql
@@ -12,7 +12,7 @@ INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (2579, 'Co
 INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (9800, 'Fagundes', 'Eduardo', 'Analista', 'MW');
 INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (1365, 'Gusmao', 'Andre', 'Estagiario', 'MW');
 INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (7789, 'Assinato', 'Raissa', 'Estagiario', 'MW');
-INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (4241, 'Lins', 'Douglas', 'Estagiario', 'SP');
-INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (2245, 'Freitas', 'Matheus', 'Analista', 'SP');
-INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (6672, 'Guth', 'Lucas', 'Estagiario', 'SP');
-INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (8889, 'Silva', 'Gabriel', 'Analista', 'SP');
+INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (4241, 'Lins', 'Douglas', 'Estagiario', 'SO');
+INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (2245, 'Freitas', 'Matheus', 'Analista', 'BD');
+INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (6672, 'Guth', 'Lucas', 'Estagiario', 'BD');
+INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (8889, 'Silva', 'Gabriel', 'Analista', 'SO');
