bd488496bcefc043aa43a311beed7e369741d8e3
mschirbel <mschirbel.g@gmail.com>
2019-05-05 00:17:04 -0300

    insert data on table + fetching it from webserver

--- httpd+mysql/database/setup.sql
+++ httpd+mysql/database/setup.sql
@@ -1,10 +1,18 @@
 USE marcelo;
-CREATE TABLE Persons (
+CREATE TABLE People (
     PersonID int,
     LastName varchar(255),
     FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255) 
+    Cargo varchar(255),
+    Team varchar(255) 
 );
-INSERT INTO Persons(PersonID, LastName, FirstName, Address, City) VALUES (2567, 'Schirbel', 'Marcelo', 'Brasil', 'SP');
-INSERT INTO Persons(PersonID, LastName, FirstName, Address, City) VALUES (3623, 'Cruz', 'Fernando', 'Brasil', 'SP');
+INSERT INTO People(PersonID, LastName, FirstName, Address, City) VALUES (2567, 'Schirbel', 'Marcelo', 'Estagiario', 'MW');
+INSERT INTO People(PersonID, LastName, FirstName, Address, City) VALUES (3623, 'Cruz', 'Fernando', 'Gestor', 'SO e MW');
+INSERT INTO People(PersonID, LastName, FirstName, Address, City) VALUES (2579, 'Corassini', 'Nilton', 'Analista', 'MW');
+INSERT INTO People(PersonID, LastName, FirstName, Address, City) VALUES (9800, 'Fagundes', 'Eduardo', 'Analista', 'MW');
+INSERT INTO People(PersonID, LastName, FirstName, Address, City) VALUES (1365, 'Gusmao', 'Andre', 'Estagiario', 'MW');
+INSERT INTO People(PersonID, LastName, FirstName, Address, City) VALUES (7789, 'Assinato', 'Raissa', 'Estagiario', 'MW');
+INSERT INTO People(PersonID, LastName, FirstName, Address, City) VALUES (4241, 'Lins', 'Douglas', 'Estagiario', 'SP');
+INSERT INTO People(PersonID, LastName, FirstName, Address, City) VALUES (2245, 'Freitas', 'Matheus', 'Analista', 'SP');
+INSERT INTO People(PersonID, LastName, FirstName, Address, City) VALUES (6672, 'Guth', 'Lucas', 'Estagiario', 'SP');
+INSERT INTO People(PersonID, LastName, FirstName, Address, City) VALUES (8889, 'Silva', 'Gabriel', 'Analista', 'SP');
--- httpd+mysql/webserver/index.php
+++ httpd+mysql/webserver/index.php
@@ -10,5 +10,18 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
+echo "Connected successfully to the Database";
+
+echo "Check the content of the table People:"
+
+$sql = "SELECT * FROM People"
+$result = $conn->query($sql);
+
+if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        echo "Nome: " .$row["FirstName"]. " - Sobrenome: " . $row["LastName"]. " - Cargo: ". $row["Cargo"]. " - Time: ". $row["Team"]. " ID: ". $row["id"]. "<br>";
+    } else {
+        echo "no results found";
+    }
+}
 ?>
