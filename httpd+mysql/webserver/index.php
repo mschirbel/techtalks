28d82efab557da5cf74c10fccd9337d34352f29c
mschirbel <mschirbel.g@gmail.com>
2019-05-05 13:26:02 -0300

    removing the sql query

--- httpd+mysql/webserver/index.php
+++ httpd+mysql/webserver/index.php
@@ -11,15 +11,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connected successfully to the Database";
-
-echo "Check the content of the table People:";
-
-$sql = "SELECT * FROM People";
-$result = $conn->query($sql);
-
-if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-        echo "Nome: " .$row["FirstName"]. " - Sobrenome: " . $row["LastName"]. " - Cargo: ". $row["Cargo"]. " - Time: ". $row["Team"]. " ID: ". $row["id"]. "<br>";
-    }
-}
 ?>
