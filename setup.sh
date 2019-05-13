5ab462883773b9e9da126f07dd243db907325bdb
Marcelo Schirbel Gomes <32197284+mschirbel@users.noreply.github.com>
2019-05-13 18:01:19 -0300

    updating compose

--- setup.sh
+++ setup.sh
@@ -35,7 +35,8 @@ echo "e sempre inicie o daemon do docker com o comando dockerd"
 
 ## Instalar docker-compose
 
-# curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+# curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+# chmod +x /usr/local/bin/docker-compose
 
 echo "troque a versão para obter a mais recente"
 
