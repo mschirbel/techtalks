edffa428ed136b8381049a47fbd888be40cbc634
Marcelo Schirbel Gomes <32197284+mschirbel@users.noreply.github.com>
2019-11-25 09:05:15 -0300

    new team

--- introDocker/httpd+mysql/database/setup.sql
+++ introDocker/httpd+mysql/database/setup.sql
@@ -6,13 +6,11 @@ CREATE TABLE People (
     Cargo varchar(255),
     Team varchar(255) 
 );
-INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (2567, 'Schirbel', 'Marcelo', 'Estagiario', 'MW');
-INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (3623, 'Cruz', 'Fernando', 'Gestor', 'SO e MW');
-INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (2579, 'Corassini', 'Nilton', 'Analista', 'MW');
-INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (9800, 'Fagundes', 'Eduardo', 'Analista', 'MW');
-INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (1365, 'Gusmao', 'Andre', 'Estagiario', 'MW');
-INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (7789, 'Assinato', 'Raissa', 'Estagiario', 'MW');
-INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (4241, 'Lins', 'Douglas', 'Estagiario', 'SO');
-INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (2245, 'Freitas', 'Matheus', 'Analista', 'BD');
-INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (6672, 'Guth', 'Lucas', 'Estagiario', 'BD');
-INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (8889, 'Santos', 'Gabriel', 'Analista', 'SO');
+INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (2567, 'Schirbel', 'Marcelo', 'SRE', 'Onboarding');
+INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (3623, 'Correia', 'Eric', 'Gestor', 'Onboarding');
+INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (2579, 'Barbosa', 'Hygo', 'SRE', 'Onboarding');
+INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (1365, 'Peres', 'Kamila', 'SRE', 'Onbarding');
+INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (7789, 'Antonio', 'Jose', 'SRE', 'Onboarding');
+INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (4241, 'Cardoso', 'Israel', 'SRE', 'Onboarding');
+INSERT INTO People(PersonID, LastName, FirstName, Cargo, Team) VALUES (2245, 'Carlos', 'Antonio', 'SRE', 'Onboarding');
+
